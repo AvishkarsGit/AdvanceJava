c1ec6897b24806fb21e24bb831c027d1fb35433a
AvishkarsGit <kumbharavishkar7@gmail.com>
2022-12-21 17:00:50 +0530

    inital commit

--- src/Swing/FrameDemo.java
+++ src/Swing/FrameDemo.java
@@ -1,13 +1,13 @@
-package Swing;
+    package Swing;
 
-import javax.swing.*;
+    import javax.swing.*;
 
-public class FrameDemo extends JFrame {
-    public static void main(String[] args) {
-        FrameDemo f = new FrameDemo();
-        f.setSize(500,500);
-        f.setVisible(true);
-        f.setTitle("Swing Frame");
-        f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    public class FrameDemo extends JFrame {
+        public static void main(String[] args) {
+            FrameDemo f = new FrameDemo();
+            f.setSize(500,500);
+            f.setVisible(true);
+            f.setTitle("Swing Frame");
+            f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        }
     }
-}
